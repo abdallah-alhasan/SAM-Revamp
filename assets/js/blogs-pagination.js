d0cdeb6f3cf6bb799315993b7fe6efd7a79e2c06
Abdallah alhasan <eng.abdallahalhasan@gmail.com>
2023-03-30 18:07:30 +0300

    Added Media

--- assets/js/blogs-pagination.js
+++ assets/js/blogs-pagination.js
@@ -1,84 +1,91 @@
-function paginateNewsCards(filteredNewsCards) {
-    const cardsPerPage = 7;
-    const paginationContainer = document.querySelector(".blog-pagination");
-    const totalPages = Math.ceil(filteredNewsCards.length / cardsPerPage);
+function filterNewsCards() {
+  const searchInput = document.querySelector("#blogs-search");
+  const newsCards = document.querySelectorAll(".blogs-modify .news-card");
+  const paginationContainer = document.querySelector(".blog-pagination");
 
-    function showPage(pageNumber) {
-        const startIndex = (pageNumber - 1) * cardsPerPage;
-        const endIndex = startIndex + cardsPerPage;
+  searchInput.addEventListener("input", (event) => {
+      const searchText = event.target.value.toLowerCase();
+      const filteredCards = [];
 
-        for (let i = 0; i < filteredNewsCards.length; i++) {
-            if (i >= startIndex && i < endIndex) {
-                filteredNewsCards[i].style.display = "grid";
-            } else {
-                filteredNewsCards[i].style.display = "none";
-            }
-        }
+      newsCards.forEach((card) => {
+          const cardTitle = card.querySelector("h3").textContent.toLowerCase();
+          const cardContent = card.querySelector("p").textContent.toLowerCase();
 
-        // Add "active" class to the active page button
-        const buttons = paginationContainer.querySelectorAll("button");
-        buttons.forEach((button) => {
-            if (Number(button.textContent) === pageNumber) {
-                button.classList.add("active");
-            } else {
-                button.classList.remove("active");
-            }
-        });
-    }
+          if (cardTitle.includes(searchText) || cardContent.includes(searchText)) {
+              card.style.display = "grid";
+              filteredCards.push(card);
+          } else {
+              card.style.display = "none";
+          }
+      });
 
-    function createPaginationButtons() {
-        for (let i = 1; i <= totalPages; i++) {
-            const button = document.createElement("button");
-            button.textContent = i;
-            button.addEventListener("click", () => showPage(i));
-            paginationContainer.appendChild(button);
-        }
-    }
+      paginateNewsCards(filteredCards, paginationContainer);
+  });
 
-    function addFirstAndLastPageButtons() {
-        const firstPageButton = document.createElement("button");
-        firstPageButton.textContent = "<<";
-        firstPageButton.addEventListener("click", () => showPage(1));
-        paginationContainer.insertBefore(firstPageButton, paginationContainer.firstChild);
-
-        const lastPageButton = document.createElement("button");
-        lastPageButton.textContent = ">>";
-        lastPageButton.addEventListener("click", () => showPage(totalPages));
-        paginationContainer.appendChild(lastPageButton);
-    }
-
-    showPage(1);
-    createPaginationButtons();
-    addFirstAndLastPageButtons();
-
-    // Make the first page button active by default
-    const buttons = paginationContainer.querySelectorAll("button");
-    buttons[1].classList.add("active");
+  paginateNewsCards(newsCards, paginationContainer);
 }
 
+function paginateNewsCards(cards, paginationContainer) {
+  const cardsPerPage = 7;
+  const totalPages = Math.ceil(cards.length / cardsPerPage);
 
-function filterNewsCards() {
-    const searchInput = document.querySelector("#blogs-search");
-    const newsCards = document.querySelectorAll(".blogs-modify .news-card");
-    const paginationContainer = document.querySelector(".blog-pagination");
+  function showPage(pageNumber) {
+      const startIndex = (pageNumber - 1) * cardsPerPage;
+      const endIndex = startIndex + cardsPerPage;
 
-    searchInput.addEventListener("input", (event) => {
-        const searchText = event.target.value.toLowerCase();
+      for (let i = 0; i < cards.length; i++) {
+          if (i >= startIndex && i < endIndex) {
+              cards[i].style.display = "grid";
+          } else {
+              cards[i].style.display = "none";
+          }
+      }
 
-        const filteredNewsCards = Array.from(newsCards).filter((card) => {
-            const cardTitle = card.querySelector("h3").textContent.toLowerCase();
-            const cardContent = card.querySelector("p").textContent.toLowerCase();
+      // Add "active" class to the active page button
+      const buttons = paginationContainer.querySelectorAll("button");
+      buttons.forEach((button) => {
+          if (Number(button.textContent) === pageNumber) {
+              button.classList.add("active");
+          } else {
+              button.classList.remove("active");
+          }
+      });
+  }
 
-            return cardTitle.includes(searchText) || cardContent.includes(searchText);
-        });
+  function createPaginationButtons() {
+      for (let i = 1; i <= totalPages; i++) {
+          const button = document.createElement("button");
+          button.textContent = i;
+          button.addEventListener("click", () => showPage(i));
+          paginationContainer.appendChild(button);
+      }
+  }
 
-        // Clear existing pagination buttons
-        paginationContainer.innerHTML = "";
+  function addFirstAndLastPageButtons() {
+      const firstPageButton = document.createElement("button");
+      firstPageButton.textContent = "<<";
+      firstPageButton.addEventListener("click", () => showPage(1));
+      paginationContainer.insertBefore(firstPageButton, paginationContainer.firstChild);
 
-        // Only create pagination buttons based on filtered news cards
-        paginateNewsCards(filteredNewsCards);
-    });
+      const lastPageButton = document.createElement("button");
+      lastPageButton.textContent = ">>";
+      lastPageButton.addEventListener("click", () => showPage(totalPages));
+      paginationContainer.appendChild(lastPageButton);
+  }
+
+  showPage(1);
+  paginationContainer.innerHTML = ""; // clear existing buttons
+  createPaginationButtons();
+  addFirstAndLastPageButtons();
+
+  // Make the first page button active by default
+  const buttons = paginationContainer.querySelectorAll("button");
+  buttons[1].classList.add("active");
 }
 
 filterNewsCards();
 
+const blogsForm = document.querySelector(".blogs-form-container form");
+blogsForm.addEventListener("submit", (event) =>{
+  event.preventDefault()
+})
